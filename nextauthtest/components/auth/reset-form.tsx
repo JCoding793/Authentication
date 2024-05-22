8dcdca42b0e1be760ed5f975ca7d6002d5cb2d99
Jatins <jatin.s@branstr.com>
2024-05-22 10:46:50 +0530

    New file Reset from

--- nextauthtest/components/auth/reset-form.tsx
+++ nextauthtest/components/auth/reset-form.tsx
@@ -18,31 +18,29 @@ import { Button } from "@/components/ui/button";
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 import { reset } from "@/actions/reset";
-import {  useTransition } from "react";
+import { useTransition } from "react";
 const ResetForm = () => {
- 
-    const [error , setError] = useState<string | undefined>("")
-    const [success , setSuccess] = useState<string | undefined>("")
-    const [isPending , startTransition] = useTransition();
- 
-    const form = useForm<z.infer<typeof ResetSchema>>({
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof ResetSchema>>({
     resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: "",
     },
   });
-  const onSubmit = (values: z.infer<typeof ResetSchema>)=>{
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError("");
     setSuccess("");
-    console.log(values)
-    startTransition(()=>{
-        
-        reset(values).then((data)=>{
-            setError(data?.error)
-            setSuccess(data?.success)
-        })
-    })
-  }
+    console.log(values);
+    startTransition(() => {
+      reset(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
   return (
     <CardWrapper
       headerLabel="Forgot your password?"
@@ -67,16 +65,15 @@ const ResetForm = () => {
                       type="email"
                     />
                   </FormControl>
-              
+
                   <FormMessage />
                 </FormItem>
               )}
             />
-         
           </div>
           <FormError message={error} />
-          <FormSuccess message={success}/>
-          
+          <FormSuccess message={success} />
+
           <Button disabled={isPending} type="submit" className="w-full">
             Send reset email
           </Button>
